15092c6aa37b868b9bf2639de204c178aae398f8
Azer Masimov <mesimovazer@gmail.com>
2022-06-25 19:30:24 +0400

    react-query-sorting-data

---
+++ src/components/Planets.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import Planet from "./Planet";
+
+const fetchPlanets = async (key) => {
+  // console.log(key.queryKey[1]);
+  const page = key.queryKey[2];
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
+  return res.json();
+};
+
+function Planets() {
+  const [page ,setPage] = useState(1);
+
+  const { data, status } = useQuery(
+    ["planets", "hello, Azar", page],
+    fetchPlanets,
+    {
+      staleTime: 0,
+      // cacheTime: 10,
+      onSuccess: () => console.log("data fetched with no problem"),
+    }
+  );
+
+  return (
+    <div>
+      <h2>Planets</h2>
+
+      <button onClick={() => setPage(1)}>page 1</button>
+      <button onClick={() => setPage(2)}>page 2</button>
+      <button onClick={() => setPage(3)}>page 3</button>
+
+      {status === "loading" && <div>Loading data...</div>}
+
+      {status === "error" && <div>Error fetching data</div>}
+
+      {status === "success" && (
+        <div>
+          {data.results.map((planet) => (
+            <Planet key={planet.name} planet={planet} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Planets;
